ba556c428a0a351884ec3827f39c64da17633e93
tsh3lley <thomasjshelley@gmail.com>
2015-11-21 02:07:51 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # YoAlert
 Send alerts to phones and (eventually) with the press of an Amazon Go Button
+
+Based on the work of Ted Benson https://medium.com/@edwardbenson/how-i-hacked-amazon-s-5-wifi-button-to-track-baby-data-794214b0bdd8#.nt2r412ex
+
+And and YoPy https://github.com/parthanium/YoPy
---
+++ src/main.py
@@ -0,0 +1,14 @@
+import yopy
+
+token = "b990f10d-d293-4f20-b14f-dcd782812bd4"
+username = "THOMASSHELLEY"
+link = ""
+location = ""
+
+yo = yopy.Yo(token)
+#print yo.number()
+yo.youser(username)
+#yo.yoall(link=link)
+#yo.yoall(location=location)
+#yo.youser(username, link=link)
+#yo.youser(username, location=location)
